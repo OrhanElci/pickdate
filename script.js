61a60b79e9611f13479f619ed6d84adf563990e7
Orhan Elci <orhan.elci2003@gmail.com>
2025-07-07 11:39:49 -0400

    Updated site with Turkish text and improved design

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function selectActivity(activity) {
   const result = document.getElementById("result");
   result.textContent = `Yay! Can't wait for our ${activity}! ❤️`;
 }
+function selectActivity(activity) {
+  const result = document.getElementById("result");
+  result.textContent = `Yaşasın! Bugün ${activity} yapıyoruz! 💞`;
+}
